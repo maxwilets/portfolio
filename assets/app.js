35ab117852ee0301adf232d9f216b07ed4f5be9a
Max Wilets <mwilet@gmail.com>
2018-10-18 15:07:17 -0600

    portfolio

--- assets/app.js
+++ assets/app.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
   $(document).ready(function () {
   
     // Hide/show animation hamburger function
-    $('.navbar-toggler').on('click', function () {
+    $('.transition').on('click', function () {
   
       // Take this line to first hamburger animations
       $('.animated-icon1').toggleClass('open');
