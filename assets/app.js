e923a9eeeca9d2b5133d1e4284e9a677d95cd2d5
Max Wilets <mwilet@gmail.com>
2018-10-18 16:58:21 -0600

    portfolio

--- assets/app.js
+++ assets/app.js
@@ -1,20 +1,39 @@
 $(document).ready(function () {
-    var open = false
-    $('.animated-icon1,.animated-icon3,.animated-icon4').click(function () {
+   // var open = false
+    //$('.animated-icon1,.animated-icon3,.animated-icon4').click(function () {
    $(this).toggleClass('open');
-   if (open === false){
-   $('.putHere').html($('.navbar-nav'))
-   $('.putHere').show()
-   console.log('test')
-   open = true
-   }
-   else if (open){
-       $('.putHere').hide()
-       open = false
-   }
-});
+   //if (open === false){
+   //$('.putHere').html($('.navbar-nav'))
+   //$('.putHere').show()
+   //console.log('test')
+   //open = true
+   //}
+   //else if (open){
+     //  $('.putHere').hide()
+       //open = false
+  // }
+//});
    
   });
   
   // Works everywhere
+
+
+  // Works everywhere
+  $(document).ready(function () {
+  
+    // Hide/show animation hamburger function
+    $('.navbar-toggler').on('click', function () {
+  
+      // Take this line to first hamburger animations
+      $('.animated-icon1').toggleClass('open');
+  
+      // Take this line to second hamburger animation
+      $('.animated-icon3').toggleClass('open');
+  
+      // Take this line to third hamburger animation
+      $('.animated-icon4').toggleClass('open');
+    });
+  
+  });
   
