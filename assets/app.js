8dbaeb8ee9eeab704a35dd0ff0431130b956c663
Max Wilets <mwilet@gmail.com>
2018-10-18 19:01:04 -0600

    fix hamburger2

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,7 @@
-$(document).ready(function () {
+//$(document).ready(function () {
    // var open = false
     //$('.animated-icon1,.animated-icon3,.animated-icon4').click(function () {
-   $(this).toggleClass('open');
+ //  $(this).toggleClass('open');
    //if (open === false){
    //$('.putHere').html($('.navbar-nav'))
    //$('.putHere').show()
@@ -14,7 +14,7 @@ $(document).ready(function () {
   // }
 //});
    
-  });
+ // });
   
   // Works everywhere
 
